90413834f289055574a30475fe19372936c67dc6
CoohCooh <saiqvo495@gmail.com>
2022-07-19 17:01:40 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,5 +6,5 @@ apt install unzip zip wget -y
 cd /app/
 
 wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.19.10.03.zip
-unzip bedrock-server-1.19.10.03.zip
+unzip -o bedrock-server-1.19.10.03.zip
 ./bedrock_server
