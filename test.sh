a9632fa15ede810307dd72142b0e7b754a919b9a
CoohCooh <saiqvo495@gmail.com>
2022-07-19 16:57:14 -0300

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt update
+apt install unzip zip wget -y
+
+cd /app/
+
+wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.19.10.03.zip
+unzip bedrock-server-1.19.10.03.zip
+./bedrock_server
